5814193ed321a2ed5d046a22078285cbde625d4e
Miguel Álvarez Díez <miguelwork92@gmail.com>
2022-04-13 21:37:33 +0200

    initial commit

---
+++ src/test/java/io/github/givimad/rustpotter-java/RustpotterJavaTest.java
@@ -0,0 +1,13 @@
+package io.github.givimad.rustpotter_java;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RustpotterJavaTest {
+    @Test public void getFrameSizeFromRust() {
+        RustpotterJava.loadLibrary();
+        RustpotterJava rustpotter = new RustpotterJava();
+        var frameSize = rustpotter.getFrameSize();
+        assertEquals("get frame size from rustpotter", 480, frameSize);
+    }
+}
